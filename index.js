adaad5a8a15089c56159286c30334b5a5133e775
Felipe Souza <felipesouza0500@gmail.com>
2023-08-22 09:04:20 -0300

    ...

--- index.js
+++ index.js
@@ -9,12 +9,6 @@ app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(cors());
 app.use('/visitantes', rotaVisitante);
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // ou '*'
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
 
 
 app.listen(4015, '0.0.0.0', ()=>{
