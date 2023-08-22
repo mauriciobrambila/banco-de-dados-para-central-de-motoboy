a07f954b5b40eeee3bdc52129e95a81e02b0e905
Felipe Souza <felipesouza0500@gmail.com>
2023-08-22 10:16:55 -0300

    ....

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(cors());
-app.use('/', rotaVisitante);
+app.use('/visitantes', rotaVisitante);
 
 
 app.listen(4015, '0.0.0.0', ()=>{
