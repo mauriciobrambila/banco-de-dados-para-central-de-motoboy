25b22fdeef7dd384711795b3d88c278017ccfdf7
Felipe Souza <felipesouza0500@gmail.com>
2023-08-23 08:31:08 -0300

    BD

--- Persistencia/Conexao.js
+++ Persistencia/Conexao.js
@@ -9,7 +9,7 @@ export default async function Conectar(){
         host:"127.0.0.1",
         user:"aluno15-pfsii",
         port:3306,
-        password:"Y0FNf7a0TpDa35TK4ZBD",
+        password:"cQbytcw5eQ9jDsXDzLyi",
         database:"backendvisitantes"
     });
 
