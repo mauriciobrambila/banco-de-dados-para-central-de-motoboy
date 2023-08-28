4e80ede723cc2fafcaa1de8e35e6486ff7916ec1
Felipe Souza <felipesouza0500@gmail.com>
2023-08-28 10:36:40 -0300

    create pool

--- Persistencia/Conexao.js
+++ Persistencia/Conexao.js
@@ -1,19 +1,37 @@
 import mysql from 'mysql2/promise';
 
 export default async function Conectar(){
-    if(global.conexao && global.conexao.status != "disconnected"){
-        return global.conexao;
+    if(global.poolconexao){
+        return await global.poolconexao.getConnection();
     }
 
-    const conexao = await mysql.createConnection({
+    const poolconexao = await mysql.createPool({
         host:"127.0.0.1",
         user:"aluno15-pfsii",
         port:3306,
         password:"2sQBdmDjddPXaobu7N5V",
-        database:"backendvisitantes"
+        database:"backendvisitantes",
+        waitForConnections: true,
+        connectionLimit: 10,
+        maxIdle: 10,
+        idleTimeout: 60000,
+        queueLimit: 0,
+        enableKeepAlive: true,
+        keepAliveInitialDelay: 0
     });
 
-    global.conexao = conexao;
-
-    return conexao;
+    global.poolconexao = poolconexao;
+    return await poolconexao.getConnection();
 }
+
+    // const conexao = await mysql.createConnection({
+    //     host:"127.0.0.1",
+    //     user:"aluno15-pfsii",
+    //     port:3306,
+    //     password:"2sQBdmDjddPXaobu7N5V",
+    //     database:"backendvisitantes"
+    // });
+
+    // global.conexao = conexao;
+
+    // return conexao;
--- Persistencia/VisitanteBD.js
+++ Persistencia/VisitanteBD.js
@@ -8,6 +8,7 @@ export default class VisitanteBD{
             const sql = "INSERT INTO newvisitantes(nome, sobrenome, cpf, rg, telefone, data, observacao) VALUES(?,?,?,?,?,?,?)";
             const valores = [visitante.nome, visitante.sobrenome, visitante.cpf, visitante.rg, visitante.telefone, visitante.data, visitante.observacao];
             const resultado = await conexao.query(sql, valores);
+            global.poolConexoes.release(conexao);
             return await resultado[0].insertId;
         }
     }
@@ -18,6 +19,7 @@ export default class VisitanteBD{
             const sql = "UPDATE newvisitantes SET nome=?, sobrenome=?, cpf=?, rg=?, telefone=?, data=?, observacao=? WHERE codigo=?";
             const valores = [visitante.nome, visitante.sobrenome, visitante.cpf, visitante.rg, visitante.telefone, visitante.data, visitante.observacao, visitante.codigo];
             await conexao.query(sql, valores);
+            global.poolConexoes.release(conexao);
         }
     }
 
@@ -27,6 +29,7 @@ export default class VisitanteBD{
             const sql = "DELETE FROM newvisitantes WHERE codigo=?";
             const valores = [visitante.codigo];
             await conexao.query(sql, valores);
+            global.poolConexoes.release(conexao);
         }
     }
 
@@ -35,6 +38,7 @@ export default class VisitanteBD{
         const sql = "SELECT * FROM newvisitantes WHERE nome LIKE ?";
         const valores = ['%' + termo + '%']
         const [rows] = await conexao.query(sql, valores);
+        global.poolConexoes.release(conexao);
         const listaVisitantes = [];
         for (const row of rows){
             const visitante = new Visitante(row['codigo'], row['nome'], row['sobrenome'], row['cpf'], row['rg'], row['telefone'], row['data'], row['observacao']);
